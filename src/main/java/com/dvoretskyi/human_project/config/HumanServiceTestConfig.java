0010611f2e36efdbcaf0e84fbec822910ce8926d
OrestDvoretskyi <Or4yk5@gmail.com>
2018-11-02 13:53:31 +0200

    add swagger an unittests

--- src/main/java/com/dvoretskyi/human_project/config/HumanServiceTestConfig.java
+++ src/main/java/com/dvoretskyi/human_project/config/HumanServiceTestConfig.java
@@ -1,18 +1,11 @@
 package com.dvoretskyi.human_project.config;
 
-import org.mockito.Mockito;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 
-@Profile("test")
-@Configuration
 public class HumanServiceTestConfig {
-@Bean
+/*@Bean
 @Primary
   public HumanServiceImla humanServiceIml(){
 
   return Mockito.mock(HumanServiceIml.class);
-}
+}*/
 }
